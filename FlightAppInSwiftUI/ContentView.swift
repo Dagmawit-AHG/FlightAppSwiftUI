cc49db820bde1ae511bfc94432e45a30d92ac754
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-04 17:33:51 +0300

    Initial Commit

---
+++ FlightAppInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlightAppInSwiftUI
+//
+//  Created by Dagmawit Alemayehu on 04/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlightAppInSwiftUI/FlightAppInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightAppInSwiftUIApp.swift
+//  FlightAppInSwiftUI
+//
+//  Created by Dagmawit Alemayehu on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlightAppInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
