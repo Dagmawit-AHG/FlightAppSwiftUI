4f12e4e5b7261c3ddca418181fb4624abc36ee25
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-14 15:00:39 +0300

    Added a SegmentedControl to pick between 'Round Trip' and 'One Way' flights.

--- FlightAppInSwiftUI/View/HomeView.swift
+++ FlightAppInSwiftUI/View/HomeView.swift
@@ -4,10 +4,10 @@
 //
 //  Created by Dagmawit Alemayehu on 09/03/2023.
 //
-
 import SwiftUI
 
 struct HomeView: View {
+    @State private var flightType = 0
     var body: some View {
         ZStack {
             Image("Start")
@@ -19,16 +19,25 @@ struct HomeView: View {
                         RoundedRectangle(cornerRadius: 20, style: .continuous)
                         .fill(Color.white)
                         .padding(.top, 170)
-                        .ignoresSafeArea()
+                        VStack (alignment: .leading){
+                            Text("Hello")
+                            .font(.system(size: 28, weight: .medium))
+                            Text("Plan your next travel")
+                            .font(.system(size: 17, weight: .regular))
+                        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                            .padding(.top, 70)
+                            .padding(15)
                         ScrollView {
-                            VStack (alignment: .leading){
-                                Text("Hello")
-                                .font(.system(size: 28, weight: .medium))
-                                Text("Plan your next travel")
-                                .font(.system(size: 17, weight: .regular))
-                            }
-                        }.padding(.top, 100)
-                            .ignoresSafeArea()
+                            VStack {
+                                Picker("", selection: $flightType) {
+                                    Text("Round Trip").tag(0)
+                                    Text("One Way").tag(1)
+                                }.pickerStyle(.segmented)
+                                
+                            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                                .padding(.top, 180)
+                                .padding(15)
+                        }
                     }
                     
                 )
