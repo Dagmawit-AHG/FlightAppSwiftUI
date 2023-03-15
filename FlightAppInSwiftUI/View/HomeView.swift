f3b4eb8df55f08701d96238968805e6854100296
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-15 11:51:31 +0300

    Added airplane images to textfields Added two date pickers with calendar images

--- FlightAppInSwiftUI/View/HomeView.swift
+++ FlightAppInSwiftUI/View/HomeView.swift
@@ -10,6 +10,9 @@ struct HomeView: View {
     @State private var flightType = 0
     @State private var fromRoundTripText: String = String()
     @State private var toRoundTripText: String = String()
+    @State private var departureDate = Date.now
+    @State private var returnDate = Date.now
+    @State private var searchFlightPressed = false
     var body: some View {
         ZStack {
             Image("Start")
@@ -35,21 +38,76 @@ struct HomeView: View {
                                     Text("Round Trip").tag(0)
                                     Text("One Way").tag(1)
                                 }.pickerStyle(.segmented)
-                                TextField("From",text: $fromRoundTripText)
+                                ZStack {
+                                    Image(systemName: "airplane.departure")
+                                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .trailing)
+                                        .padding()
+                                    TextField("From",text: $fromRoundTripText)
                                     .frame(height: 50)
                                     .padding()
                                     .border(.clear)
                                     .overlay(RoundedRectangle(cornerRadius: 5)
                                         .strokeBorder(Color.accentColor)
                                     )
-                                TextField("To",text: $toRoundTripText)
+                                }
+                                ZStack {
+                                    Image(systemName: "airplane.arrival")
+                                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .trailing)
+                                        .padding()
+                                    TextField("To",text: $toRoundTripText)
                                     .frame(height: 50)
                                     .padding()
                                     .border(.clear)
                                     .overlay(RoundedRectangle(cornerRadius: 5)
                                         .strokeBorder(Color.accentColor)
                                     )
-                                
+                                    
+                                }
+                                HStack(spacing: 30) {
+                                    
+                                    ZStack {
+                                        Image(systemName: "calendar")
+                                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .bottomLeading)
+                                        .padding(5)
+                                        DatePicker(selection: $departureDate, in: ...Date.now, displayedComponents: .date) {
+                                            
+                                    }
+                                    .frame(width: 110,height: 70)
+                                    .padding()
+                                    .overlay(RoundedRectangle(cornerRadius: 5)
+                                        .strokeBorder(Color.accentColor)
+                                    )
+                                    }
+                                    ZStack{
+                                        Image(systemName: "calendar")
+                                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .bottomLeading)
+                                        .padding(5)
+                                        DatePicker(selection: $returnDate, in: ...Date.now, displayedComponents: .date) {
+
+                                    }
+                                    .frame(width: 110,height: 70)
+                                    .padding()
+                                    .overlay(RoundedRectangle(cornerRadius: 5)
+                                        .strokeBorder(Color.accentColor)
+                                    )
+                                    }
+                                }
+                                Button(action: { searchFlightPressed = true }, label: {
+                                    Text("Search Flights")
+                                    .font(.headline)
+                                    .fontWeight(.regular)
+                                    .foregroundColor(.white)
+                                    .frame(width: 315, height: 49)
+                                    .background(Color.accentColor)
+                                    .cornerRadius(5)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(lineWidth: 1)
+                                            .foregroundColor(Color.accentColor)
+                                            .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                                    )
+                                })
+                                .padding(.top, 16)
                             }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
                                 .padding(.top, 180)
                                 .padding(30)
