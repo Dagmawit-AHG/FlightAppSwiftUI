4f040d3fd089679e93d6ae1e34a488a834d9cb7a
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-17 11:47:07 +0300

    Added Segmented Picker with onChange property

--- FlightAppInSwiftUI/View/HomeView.swift
+++ FlightAppInSwiftUI/View/HomeView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var selectedSegment = 0
     var body: some View {
         ZStack {
             Image("Start")
@@ -20,15 +21,27 @@ struct HomeView: View {
                         .fill(Color.white)
                         .padding(.top, 170)
                         .ignoresSafeArea()
-                        ScrollView {
-                            VStack (alignment: .leading){
-                                Text("Hello")
-                                .font(.system(size: 28, weight: .medium))
-                                Text("Plan your next travel")
-                                .font(.system(size: 17, weight: .regular))
-                            }
-                        }.padding(.top, 100)
-                            .ignoresSafeArea()
+                        VStack (alignment: .leading){
+                            Text("Hello")
+                            .font(.system(size: 28, weight: .medium))
+                            Text("Plan your next travel")
+                            .font(.system(size: 17, weight: .regular))
+                        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                            .padding(.top, 70)
+                            .padding(15)
+                        VStack(spacing: 30) {
+                            Picker("",selection: $selectedSegment){
+                                Text("Round Trip").tag(0)
+                                Text("One Way").tag(1)
+                            }.pickerStyle(.segmented)
+                                .onChange(of: selectedSegment) { s in
+                                    
+                                }
+                            RoundTripView()
+                        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                        .padding(.top, 180)
+                        .padding(30)
+                            
                     }
                     
                 )
