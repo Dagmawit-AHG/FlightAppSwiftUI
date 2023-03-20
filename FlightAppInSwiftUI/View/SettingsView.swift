cc80a6caaa6ea57a83cecaa5e7ec4d6153082201
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-20 09:46:08 +0300

    Added NavBar buttons and Title to the settings screen

--- FlightAppInSwiftUI/View/SettingsView.swift
+++ FlightAppInSwiftUI/View/SettingsView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var settingsIconPressed = false
+    @State private var backIconPressed = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("Settings")
+                    .font(.system(size: 28,weight: .medium))
+                    .padding(.top,100)
+                    .padding(.leading,30)
+            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                .ignoresSafeArea()
+            
+                .font(.system(size: 28,weight: .medium))
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        Button(action: { settingsIconPressed = true }) {
+                            Image(systemName: "gearshape")
+                                .foregroundColor(.black)
+                                .font(.title2)
+                        }
+                    }
+                    ToolbarItemGroup(placement: .navigationBarLeading) {
+                        Button(action: { backIconPressed = true }) {
+                            Image(systemName: "arrow.backward")
+                                .foregroundColor(.black)
+                                .font(.title2)
+                        }
+                    }
+                }
+            NavigationLink("", destination: SettingsView(), isActive: $settingsIconPressed)
+        }
     }
 }
 
