718ebcd5cb2b43857d601a376aa56e7f5828062d
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-20 11:40:45 +0300

    Implemented the custom card views in SettingsView. Also added buttons to the card views.

--- FlightAppInSwiftUI/View/SettingsView.swift
+++ FlightAppInSwiftUI/View/SettingsView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var showLanguagesView = false
+    @State private var darkModeIsOn = false
+    @State private var notificationsIsOn = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:30) {
+            ZStack{
+                SettingsCardView(image: "character.bubble", title: "Language")
+                Button("English(UK)  >", action: { showLanguagesView = true})
+                    .font(.system(size: 16,weight: .light))
+                    .foregroundColor(Color.black)
+                    .frame(width: 340, height: 98, alignment: .trailing)
+                    .padding(.trailing, 30)
+            }
+            ZStack{
+                SettingsCardView(image: "moon", title: "Dark Mode")
+                Toggle("", isOn: $darkModeIsOn)
+                    .toggleStyle(SwitchToggleStyle(tint: .black))
+                    .padding(.trailing, 20)
+            }
+            ZStack{
+                SettingsCardView(image: "bell", title: "Notifications")
+                Toggle("", isOn: $notificationsIsOn)
+                    .toggleStyle(SwitchToggleStyle(tint: .black))
+                    .padding(.trailing, 20)
+            }
+        }.padding()
     }
 }
 
