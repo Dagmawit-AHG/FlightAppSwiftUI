7f7c8015177a44bad130cba0a39a9ce0f13f858f
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-20 13:54:34 +0300

    Added big 'notifications' card view for when Notifications in 'ON'

--- FlightAppInSwiftUI/View/SettingsView.swift
+++ FlightAppInSwiftUI/View/SettingsView.swift
@@ -11,6 +11,10 @@ struct SettingsView: View {
     @State private var showLanguagesView = false
     @State private var darkModeIsOn = false
     @State private var notificationsIsOn = false
+    @State private var flightUpdatesIsOn = false
+    @State private var executiveIsOn = false
+    @State private var discountIsOn = false
+    
     var body: some View {
         ScrollView{
             VStack(spacing:30) {
@@ -28,13 +32,54 @@ struct SettingsView: View {
                         .toggleStyle(SwitchToggleStyle(tint: .black))
                         .padding(.trailing, 30)
                 }
-                ZStack{
-                    SettingsCardView(image: "bell", title: "Notifications")
-                    Toggle("", isOn: $notificationsIsOn)
-                        .toggleStyle(SwitchToggleStyle(tint: .black))
-                        .padding(.trailing, 30)
-                    
-                }
+                
+                Color.clear
+                    .overlay(
+                        ZStack {
+                            Image("Rectangle_big")
+                                .resizable()
+                            ZStack{
+                                SettingsCardView(image: "bell", title: "Notifications")
+                                Toggle("", isOn: $notificationsIsOn)
+                                    .toggleStyle(SwitchToggleStyle(tint: .black))
+                                    .padding(.trailing, 30)
+                            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
+                            VStack(spacing: 40) {
+                                HStack {
+                                    Text("Flight updates")
+                                        .font(.system(size: 18, weight: .medium))
+                                    Toggle("", isOn: $flightUpdatesIsOn)
+                                        .labelsHidden()
+                                        .toggleStyle(SwitchToggleStyle(tint: .black))
+                                        .padding(.leading, 70)
+                                }
+                                .frame(minWidth: 0, maxWidth: .infinity,alignment: .leading)
+                                .padding(.top, 130)
+                                .padding(.leading, 70)
+                                HStack {
+                                    Text("Executive program")
+                                        .font(.system(size: 18, weight: .medium))
+                                        .padding(.trailing, 35)
+                                    Toggle("", isOn: $executiveIsOn)
+                                        .labelsHidden()
+                                        .toggleStyle(SwitchToggleStyle(tint: .black))
+                                }.frame(minWidth: 0, maxWidth: .infinity,alignment: .leading)
+                                    .padding(.leading, 70)
+                                HStack {
+                                    Text("Discount & deals")
+                                        .font(.system(size: 18, weight: .medium))
+                                        .padding(.trailing, 50)
+                                    Toggle("", isOn: $discountIsOn)
+                                        .labelsHidden()
+                                        .toggleStyle(SwitchToggleStyle(tint: .black))
+                                }.frame(minWidth: 0, maxWidth: .infinity,alignment: .leading)
+                                    .padding(.leading, 70)
+                            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                        }
+                    )
+                    .clipped()
+                    .frame(width: 340, height: 343)
+                
             }
         }.padding()
     }
