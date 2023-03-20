2ab82827358d567986152e013af1754661151be7
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-20 12:05:55 +0300

    Added scrollview to the screen

--- FlightAppInSwiftUI/View/SettingsView.swift
+++ FlightAppInSwiftUI/View/SettingsView.swift
@@ -12,26 +12,29 @@ struct SettingsView: View {
     @State private var darkModeIsOn = false
     @State private var notificationsIsOn = false
     var body: some View {
-        VStack(spacing:30) {
-            ZStack{
-                SettingsCardView(image: "character.bubble", title: "Language")
-                Button("English(UK)  >", action: { showLanguagesView = true})
-                    .font(.system(size: 16,weight: .light))
-                    .foregroundColor(Color.black)
-                    .frame(width: 340, height: 98, alignment: .trailing)
-                    .padding(.trailing, 30)
-            }
-            ZStack{
-                SettingsCardView(image: "moon", title: "Dark Mode")
-                Toggle("", isOn: $darkModeIsOn)
-                    .toggleStyle(SwitchToggleStyle(tint: .black))
-                    .padding(.trailing, 20)
-            }
-            ZStack{
-                SettingsCardView(image: "bell", title: "Notifications")
-                Toggle("", isOn: $notificationsIsOn)
-                    .toggleStyle(SwitchToggleStyle(tint: .black))
-                    .padding(.trailing, 20)
+        ScrollView{
+            VStack(spacing:30) {
+                ZStack{
+                    SettingsCardView(image: "character.bubble", title: "Language")
+                    Button("English(UK)  >", action: { showLanguagesView = true})
+                        .font(.system(size: 16,weight: .light))
+                        .foregroundColor(Color.black)
+                        .frame(width: 340, height: 98, alignment: .trailing)
+                        .padding(.trailing, 30)
+                }
+                ZStack{
+                    SettingsCardView(image: "moon", title: "Dark Mode")
+                    Toggle("", isOn: $darkModeIsOn)
+                        .toggleStyle(SwitchToggleStyle(tint: .black))
+                        .padding(.trailing, 30)
+                }
+                ZStack{
+                    SettingsCardView(image: "bell", title: "Notifications")
+                    Toggle("", isOn: $notificationsIsOn)
+                        .toggleStyle(SwitchToggleStyle(tint: .black))
+                        .padding(.trailing, 30)
+                    
+                }
             }
         }.padding()
     }
