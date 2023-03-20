b9c0326261124853e085d463dbbc3f0b4838e364
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-20 10:31:02 +0300

    Created new flle for custom card view

---
+++ FlightAppInSwiftUI/Utils/SettingsCardView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsCardView.swift
+//  FlightAppInSwiftUI
+//
+//  Created by Dagmawit Alemayehu on 20/03/2023.
+//
+
+import SwiftUI
+
+struct SettingsCardView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SettingsCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsCardView()
+    }
+}
