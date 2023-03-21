e77056090616bcd54f838ea653a2268c636064d1
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-21 15:24:37 +0300

    Added navbar buttons and title with their functionalities to the languages page.

--- FlightAppInSwiftUI/View/LanguagesView.swift
+++ FlightAppInSwiftUI/View/LanguagesView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct LanguagesView: View {
+    @State private var settingsIconPressed = false
+    @State private var backIconPressed = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text("")
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Text("Language")
+                            .font(.system(size: 20, weight: .medium))
+                    }
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        Button(action: { settingsIconPressed = true }) {
+                            Image(systemName: "gearshape")
+                                .foregroundColor(.black)
+                                .font(.title2)
+                            NavigationLink("",destination: SettingsView(), isActive: $settingsIconPressed)
+                        }
+                    }
+                    ToolbarItemGroup(placement: .navigationBarLeading) {
+                        Button(action: { backIconPressed = true}) {
+                            Image(systemName: "arrow.backward")
+                                .foregroundColor(.black)
+                                .font(.title2)
+                            NavigationLink("",destination: SettingsView(), isActive: $backIconPressed)
+                        }
+                    }
+                }
+            
+        }
     }
 }
 
