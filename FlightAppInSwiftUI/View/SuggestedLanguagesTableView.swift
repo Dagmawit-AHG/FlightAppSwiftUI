7404c562f70e564ee4ab84637b7831446b0cbdde
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-21 18:20:49 +0300

    Added a background image to the list view and text below it.

--- FlightAppInSwiftUI/View/SuggestedLanguagesTableView.swift
+++ FlightAppInSwiftUI/View/SuggestedLanguagesTableView.swift
@@ -9,26 +9,40 @@ import SwiftUI
 
 struct SuggestedLanguagesTableView: View {
     var body: some View {
-        List {
-            Section {
-                LanguageCell(
-                imageName: "",
-                title: "English(US)",
-                subtitle: "Default"
-                )
-                LanguageCell(
-                    imageName: "",
-                    title: "English",
-                    subtitle: "English")
-                LanguageCell(
-                    imageName: "checkmark",
-                    title: "English(UK)",
-                    subtitle: "English(UK)")
-            } header: {
-                Text("Suggested Languages")
-                    .font(.system(size: 17, weight: .light))
+        ZStack {
+            List {
+                Section {
+                    LanguageCell(
+                        imageName: "",
+                        title: "English(US)",
+                        subtitle: "Default"
+                    )
+                    LanguageCell(
+                        imageName: "",
+                        title: "English",
+                        subtitle: "English")
+                    LanguageCell(
+                        imageName: "checkmark",
+                        title: "English(UK)",
+                        subtitle: "English(UK)")
+                } header: {
+                    Text("Suggested Languages")
+                        .font(.system(size: 17, weight: .light))
+                }.background(.clear)
             }
-        }
+            VStack {
+                Image("Rectangle_sugg")
+                .resizable()
+                .frame(width: 343, height: 213, alignment: .top)
+                
+//                .padding()
+            Text("App will use the first language that it supports from Language & Region settings. You can select a different language for App to use if you prefer.")
+                .font(.system(size: 15, weight: .light))
+                .padding()
+                
+            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
+                .padding(.top,55)
+        }.ignoresSafeArea()
     }
 }
 
