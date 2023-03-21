6b488fa34887757e788fc0040f145c12f31e54f8
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-21 16:11:15 +0300

    Created custom tableview cell

---
+++ FlightAppInSwiftUI/Utils/LanguageCell.swift
@@ -0,0 +1,35 @@
+//
+//  LanguageCell.swift
+//  FlightAppInSwiftUI
+//
+//  Created by Dagmawit Alemayehu on 21/03/2023.
+//
+
+import SwiftUI
+
+struct LanguageCell: View {
+    let imageName: String
+    let title: String
+    let subtitle: String
+    
+    var body: some View {
+        HStack(spacing: 100) {
+            VStack(alignment: .leading) {
+                Text(title)
+                    .font(.system(size: 18, weight: .medium))
+                Text(subtitle)
+                    .font(.system(size: 14, weight: .light))
+            }
+            Image(systemName: imageName)
+                .font(.headline)
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+        }
+    }
+}
+
+struct LanguageCell_Previews: PreviewProvider {
+    static var previews: some View {
+        LanguageCell(imageName: "checkmark", title: "English(US)", subtitle: "Default")
+    }
+}
