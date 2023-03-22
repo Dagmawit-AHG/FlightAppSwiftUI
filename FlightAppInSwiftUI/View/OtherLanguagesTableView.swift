85a4dda1714fc31b9caf479ea8dabf523fb84d60
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-22 11:04:39 +0300

    Added Language cells to the list

--- FlightAppInSwiftUI/View/OtherLanguagesTableView.swift
+++ FlightAppInSwiftUI/View/OtherLanguagesTableView.swift
@@ -10,7 +10,38 @@ import SwiftUI
 struct OtherLanguagesTableView: View {
     var body: some View {
         ZStack {
-            
+            List {
+                Section {
+                    LanguageCell(
+                        imageName: "",
+                        title: "Dansk",
+                        subtitle: "Danish"
+                    )
+                    LanguageCell(
+                        imageName: "",
+                        title: "Netherlands",
+                        subtitle: "Dutch")
+                    LanguageCell(
+                        imageName: "",
+                        title: "English(Canada)",
+                        subtitle: "English(Canada)")
+                    LanguageCell(
+                        imageName: "",
+                        title: "English(Australia)",
+                        subtitle: "English(Australia)")
+                    LanguageCell(
+                        imageName: "",
+                        title: "English(United States)",
+                        subtitle: "English(United States)")
+                    LanguageCell(
+                        imageName: "",
+                        title: "Italian",
+                        subtitle: "Italy")
+                } header: {
+                    Text("Other Languages")
+                        .font(.system(size: 17, weight: .light))
+                }.background(.clear)
+            }
         }
     }
 }
