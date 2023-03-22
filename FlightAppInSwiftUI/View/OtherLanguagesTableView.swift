64bd8d6591d39417b33f8d0ee82abb6f520908a4
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-22 11:01:06 +0300

    Created a view for the 'Other Languages' table

---
+++ FlightAppInSwiftUI/View/OtherLanguagesTableView.swift
@@ -0,0 +1,22 @@
+//
+//  OtherLanguagesTableView.swift
+//  FlightAppInSwiftUI
+//
+//  Created by Dagmawit Alemayehu on 22/03/2023.
+//
+
+import SwiftUI
+
+struct OtherLanguagesTableView: View {
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+struct OtherLanguagesTableView_Previews: PreviewProvider {
+    static var previews: some View {
+        OtherLanguagesTableView()
+    }
+}
