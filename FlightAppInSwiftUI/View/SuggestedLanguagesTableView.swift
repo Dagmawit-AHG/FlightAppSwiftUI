0ad53436a7cddabfb08f27376e64c8307c8c69a3
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-23 14:13:07 +0300

    Made an array of LanguageCells for the 'List' view to iterate through

--- FlightAppInSwiftUI/View/SuggestedLanguagesTableView.swift
+++ FlightAppInSwiftUI/View/SuggestedLanguagesTableView.swift
@@ -8,41 +8,47 @@
 import SwiftUI
 
 struct SuggestedLanguagesTableView: View {
+    @State private var selection: LanguageCell?
+    private var languages = [
+        LanguageCell(
+            imageName: "checkmark",
+            title: "English(US)",
+            subtitle: "Default"
+        ),
+        LanguageCell(
+            imageName: "",
+            title: "English",
+            subtitle: "English"),
+        LanguageCell(
+            imageName: "",
+            title: "English(UK)",
+            subtitle: "English(UK)")
+    ]
+    
     var body: some View {
         ZStack {
-            List {
-                Section {
-                    LanguageCell(
-                        imageName: "",
-                        title: "English(US)",
-                        subtitle: "Default"
-                    )
-                    LanguageCell(
-                        imageName: "",
-                        title: "English",
-                        subtitle: "English")
-                    LanguageCell(
-                        imageName: "checkmark",
-                        title: "English(UK)",
-                        subtitle: "English(UK)")
-                } header: {
-                    Text("Suggested Languages")
-                        .font(.system(size: 17, weight: .light))
-                }.background(.clear)
+            List(languages, id: \.self) { index in
+                HStack {
+                    index
+                }.contentShape(Rectangle())
+                    .onTapGesture {
+                        print(index)
+                    }
             }
             VStack {
                 Image("Rectangle_sugg")
                 .resizable()
+                .cornerRadius(10)
                 .frame(width: 343, height: 213, alignment: .top)
                 
-//                .padding()
             Text("App will use the first language that it supports from Language & Region settings. You can select a different language for App to use if you prefer.")
                 .font(.system(size: 15, weight: .light))
                 .padding()
                 
             }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
-                .padding(.top,55)
-        }.ignoresSafeArea()
+                .padding(.top,34)
+        }.background(.clear)
+        .ignoresSafeArea()
     }
 }
 
