45f8d6ca8a353548e62cc54af77924eb87351a4c
Dagmawit Alemayehu <dagmawit@qtf.io>
2023-03-24 10:29:11 +0300

    UI of 'Suggested Table view' still needs fixes

--- FlightAppInSwiftUI/View/SuggestedLanguagesTableView.swift
+++ FlightAppInSwiftUI/View/SuggestedLanguagesTableView.swift
@@ -30,24 +30,27 @@ struct SuggestedLanguagesTableView: View {
             List(languages, id: \.self) { index in
                 HStack {
                     index
-                }.contentShape(Rectangle())
-                    .onTapGesture {
-                        print(index)
-                    }
+                }
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    print(index)
+                }
             }
+            .frame(width: 343, height: 260, alignment: .top)
+            .ignoresSafeArea()
             VStack {
                 Image("Rectangle_sugg")
                 .resizable()
                 .cornerRadius(10)
                 .frame(width: 343, height: 213, alignment: .top)
+                Text("App will use the first language that it supports from Language & Region settings. You can select a different language for App to use if you prefer.")
+                    .font(.system(size: 15, weight: .light))
+                    .padding()
                 
-            Text("App will use the first language that it supports from Language & Region settings. You can select a different language for App to use if you prefer.")
-                .font(.system(size: 15, weight: .light))
-                .padding()
-                
-            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
-                .padding(.top,34)
-        }.background(.clear)
+            }
+        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
+            .padding(.top,34)
+        .background(.clear)
         .ignoresSafeArea()
     }
 }
